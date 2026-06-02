0eeb469ed19579464c96b1ebd7617e4f1c6a6740
Neomoon <costamarcos404@gmail.com>
2026-06-02 10:18:33 -0300

    basic 'tasks' route with POST and GET endpoints

--- src/app/main.py
+++ src/app/main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
-# from app.schemas import TaskCreate, TaskRead # import classes
+from .schemas import TaskCreate, TaskRead # import classes
 
 app = FastAPI(title="Tevira-AI")
 
-# tasks: list[TaskRead] = []
+tasks: list[TaskRead] = []
 
 @app.get("/health")
 def check_health():
     return {"status": "ok", "service": "tevira-ai"}
+
+@app.post("/tasks", status_code=201)
+def create_task(task: TaskCreate) -> TaskRead:
+    task_id = f"task_{len(tasks) + 1}"
+
+    new_task = TaskRead(
+        **task.model_dump(), # Dumps all `task` fields here, no need to type them manually.
+        id=task_id,
+        status="open",
+    )
+
+    tasks.append(new_task)
+
+    return new_task
+
+@app.get("/tasks")
+def show_tasks() -> list[TaskRead]:
+    return tasks
