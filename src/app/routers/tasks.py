f697798f78bbe9852ac230e1804596767de68e27
neomoon007 <costamarcos404@gmail.com>
2026-07-03 12:37:47 -0300

    refactored tasks.py (router) and utils.py into tasks.py (services)

--- src/app/routers/tasks.py
+++ src/app/routers/tasks.py
@@ -1,67 +1,33 @@
-from src.app.state.memory import tasks_in_memory, task_id_number
-from src.app.utils import get_project, get_project_tasks, get_task_by_id
+from src.app.services.tasks import (
+    create_task,
+    show_tasks,
+    update_task,
+    delete_task,
+)
 from fastapi import APIRouter
-from src.app.schemas import TaskCreate, TaskRead, TaskUpdate, NonEmptyString
+from src.app.schemas import (
+    TaskCreate,
+    TaskRead,
+    TaskUpdate,
+    NonEmptyString,
+)
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 
 # -- "/tasks" --
 @router.post("", status_code=201)
-def create_task(task: TaskCreate) -> TaskRead:
-    global task_id_number
-    task_id_number += 1
-
-    task_id = f"task_{task_id_number}"
-
-    new_task = TaskRead(
-        **task.model_dump(),  # Dumps all `task` fields here, no need to type them manually.
-        id=task_id,
-        status="open",
-    )
-
-    tasks_in_memory.append(new_task)
-
-    return new_task
-
+def create_task_endpoint(task: TaskCreate) -> TaskRead:
+    return create_task(task)
 
 @router.get("")
-def show_tasks(project_id: str = None, task_id: str = None) -> list[TaskRead]:  # type: ignore
-    if project_id is None and task_id is None:
-        return tasks_in_memory
-
-    if project_id is not None and task_id is None:
-        get_project(project_id)
-        return get_project_tasks(project_id)
-
-    if project_id is None and task_id is not None:
-        return [get_task_by_id(task_id)]
-
-    if project_id is not None and task_id is not None:
-        get_project(project_id)
-        project_tasks = get_project_tasks(project_id)
-        return [get_task_by_id(task_id, project_tasks)]
-
+def show_tasks_endpoint(project_id: str | None = None, task_id: str | None = None) -> list[TaskRead]:
+    return show_tasks(project_id, task_id)
 
 @router.patch("/{task_id}")
-def update_task(task_id: NonEmptyString, updated_task: TaskUpdate) -> TaskRead:
-    matching_task = get_task_by_id(task_id)
-    merged_task = {
-        **matching_task.model_dump(),
-        **updated_task.model_dump(exclude_none=True),
-    }
-
-    task_index = tasks_in_memory.index(matching_task)
-    tasks_in_memory[task_index] = TaskRead(**merged_task)
-
-    return TaskRead(**merged_task)
-
+def update_task_endpoint(task_id: NonEmptyString, updated_task: TaskUpdate) -> TaskRead:
+    return update_task(task_id, updated_task)
 
 @router.delete("/{task_id}", status_code=204)
-def delete_task(task_id: str):
-    # TODO: change the parameter from str type to a validation (similar to Depends(validate_project_id(project_id) or NonEmptyString at least)
-    matching_task = get_task_by_id(task_id)
-    task_index = tasks_in_memory.index(matching_task)
-    del tasks_in_memory[task_index]
-
-    return
+def delete_task_endpoint(task_id: str) -> None:
+    delete_task(task_id)
---
+++ src/app/services/tasks.py
@@ -0,0 +1,90 @@
+from src.app.schemas import TaskCreate, TaskRead, NonEmptyString, TaskUpdate
+from src.app.state.memory import task_id_number, tasks_in_memory
+from src.app.services.projects import get_project
+from fastapi import HTTPException
+
+def get_project_tasks(project_id) -> list[TaskRead]:
+    return [
+        task
+        for task in tasks_in_memory
+        if task.project_id == project_id and task.status == "open"
+    ]
+
+def get_task_by_id(task_id: str, database: list | None = None) -> TaskRead:
+    tasks_list = database if database is not None else tasks_in_memory
+
+    matching_task = next((task for task in tasks_list if task.id == task_id), None)
+    if not matching_task:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Error 404: Task '{task_id}' does not exist.",
+        )
+    return matching_task
+
+def get_important_task(project_id: str) -> TaskRead | str:
+    tasks_db = get_project_tasks(project_id)
+    priority_list = ["high", "medium", "low"]
+    recommended_task: TaskRead | None = None
+
+    for p in priority_list:
+        recommended_task = next(
+            (task for task in tasks_db if task.priority == p and task.status == "open"),
+            None,
+        )
+
+        if recommended_task is not None:
+            return recommended_task
+
+    return "No open next action found."
+
+def create_task(task: TaskCreate) -> TaskRead:
+    global task_id_number
+    task_id_number += 1
+
+    task_id = f"task_{task_id_number}"
+
+    new_task = TaskRead(
+        **task.model_dump(),  # Dumps all `task` fields here, no need to type them manually.
+        id=task_id,
+        status="open",
+    )
+
+    tasks_in_memory.append(new_task)
+
+    return new_task
+
+
+def show_tasks(project_id: str | None = None, task_id: str | None = None) -> list[TaskRead]:  # type: ignore
+    if project_id is None and task_id is None:
+        return tasks_in_memory
+
+    if project_id is not None and task_id is None:
+        get_project(project_id)
+        return get_project_tasks(project_id)
+
+    if project_id is None and task_id is not None:
+        return [get_task_by_id(task_id)]
+
+    if project_id is not None and task_id is not None:
+        get_project(project_id)
+        project_tasks = get_project_tasks(project_id)
+        return [get_task_by_id(task_id, project_tasks)]
+    
+
+def update_task(task_id: NonEmptyString, updated_task: TaskUpdate) -> TaskRead:
+    matching_task = get_task_by_id(task_id)
+    merged_task = {
+        **matching_task.model_dump(),
+        **updated_task.model_dump(exclude_none=True),
+    }
+
+    task_index = tasks_in_memory.index(matching_task)
+    tasks_in_memory[task_index] = TaskRead(**merged_task)
+
+    return TaskRead(**merged_task)
+
+def delete_task(task_id: NonEmptyString) -> None:
+    matching_task = get_task_by_id(task_id)
+    task_index = tasks_in_memory.index(matching_task)
+    del tasks_in_memory[task_index]
+    # TODO: change the parameter from str type to a validation (similar to Depends(validate_project_id(project_id) or NonEmptyString at least)
