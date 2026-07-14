292e8f4f7b978877ed08271b221835786a092ed4
Neomoon <costamarcos404@gmail.com>
2026-07-14 16:49:14 -0300

    Added initial database connection

---
+++ src/app/db/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is missing")
+
+engine = create_engine(DATABASE_URL)
---
+++ src/app/db/db_checker.py
@@ -0,0 +1,19 @@
+from src.app.db.database import engine
+from sqlalchemy import text
+
+
+def check_db_health():
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT version();"))
+            db_version = result.fetchone()
+
+            print("Database connection is working!")
+            print(f"Postgresql version: {db_version}")
+
+    except Exception as exception:
+        print("Database connection is NOT working!")
+        print(f"Error details: {exception}")
+
+
+check_db_health()
