7891441ee9024be3a53e7a23a5e7d1f4249324db
Neomoon <costamarcos404@gmail.com>
2026-07-20 14:05:09 -0300

    Added logs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# colors
+RESET_COLOR="\033[0m"
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+
 # Create and activate python's virtual environment
 python3 -m venv .venv
 source .venv/bin/activate
@@ -10,5 +15,5 @@ cp .env.example .env
 # Install python dependencies
 pip3 install -r requirements.txt
 
-# Tell user to change .env
-echo "\nRemember to change .env variables to customize your settings"
+echo "\n${GREEN}[Setup completed successfully! Your environment is ready.]${RESET_COLOR}"
+echo "\n${GREEN}Remember to change .env variables to customize your settings.]${RESET_COLOR}"
