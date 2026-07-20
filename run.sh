5649078e41515c8abe83d636dd220e02f8af4fbb
Neomoon <costamarcos404@gmail.com>
2026-07-20 18:00:15 -0300

    Updated run.sh to wait for postgresql before running alembic

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Stop script if error
+set -e
+
 # Colors
 RESET_COLOR="\033[0m"
 # RED="\033[0;31m"
@@ -21,11 +24,19 @@ log_info "Initializing docker containers..."
 docker compose up -d
 
 log_info "Waiting for PostgreSQL..."
-
-# until docker exec tevira-postgres
+until docker exec "$CONTAINER_NAME" \
+    pg_isready \
+    -U "$POSTGRES_USERNAME" \
+    -d "$POSTGRES_DB" \
+    >/dev/null 2>&1
+do
+    sleep 1
+done
+log_info "PostgreSQL is ready."
 
 log_info "Running alembic migrations..."
 alembic upgrade head
+log_info "Database is up to date."
 
 log_info "Initializing uvicorn server..."
 uvicorn src.app.main:app --reload
