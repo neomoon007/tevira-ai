f78453fdac1e684ef6e33e180c26c07410ae4fc4
Neomoon <costamarcos404@gmail.com>
2026-07-20 14:52:27 -0300

    formatting

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ GREEN="\033[0;32m"
 # YELLOW="\033[0;33m"
 
 # Log functions
-log_info() { echo -e "${GREEN}INFO:${RESET_COLOR} $*"; }
-# log_warn() { echo -e "${YELLOW}WARN:${RESET_COLOR}  $*"; }
-# log_error() { echo -e "${RED}ERROR:${RESET_COLOR} $*"; }
+log_info() { echo -e "${GREEN}INFO${RESET_COLOR}:     $*"; }
+# log_warn() { echo -e "${YELLOW}WARN${RESET_COLOR}:     $*"; }
+# log_error() { echo -e "${RED}ERROR${RESET_COLOR}:     $*"; }
 
 log_info "Initializing docker containers..."
 docker compose up -d
 
-log_info "Initializing uvicorn server on port 8000..."
+log_info "Initializing uvicorn server..."
 uvicorn src.app.main:app --reload
 
--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ GREEN="\033[0;32m"
 # YELLOW="\033[0;33m"
 
 # Log functions
-log_info() { echo -e "${GREEN}INFO:${RESET_COLOR} $*"; }
-# log_warn() { echo -e "${YELLOW}WARN:${RESET_COLOR}  $*"; }
-# log_error() { echo -e "${RED}ERROR:${RESET_COLOR} $*"; }
+log_info() { echo -e "${GREEN}INFO${RESET_COLOR}:     $*"; }
+# log_warn() { echo -e "${YELLOW}WARN${RESET_COLOR}:     $*"; }
+# log_error() { echo -e "${RED}ERROR${RESET_COLOR}:     $*"; }
 
 log_info "Creating python virtual environment..."
 python3 -m venv .venv
