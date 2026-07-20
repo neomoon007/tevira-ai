87460faad81e65a0367e499e430b7bc53d3af122
Neomoon <costamarcos404@gmail.com>
2026-07-20 17:43:39 -0300

    now container name is configurable on .env

--- run.sh
+++ run.sh
@@ -6,13 +6,23 @@ RESET_COLOR="\033[0m"
 GREEN="\033[0;32m"
 # YELLOW="\033[0;33m"
 
+
 # Log functions
 log_info() { echo -e "${GREEN}INFO${RESET_COLOR}:     $*"; }
 # log_warn() { echo -e "${YELLOW}WARN${RESET_COLOR}:     $*"; }
 # log_error() { echo -e "${RED}ERROR${RESET_COLOR}:     $*"; }
 
+log_info "Loading .env variables..."
+set -a
+source .env
+set +a
+
 log_info "Initializing docker containers..."
 docker compose up -d
+
+log_info "Waiting for PostgreSQL..."
+
+# until docker exec tevira-postgres
 
 log_info "Running alembic migrations..."
 alembic upgrade head
