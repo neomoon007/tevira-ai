d2721d37ddc41eaa331a82b426f9f7705b454b41
Neomoon <costamarcos404@gmail.com>
2026-07-20 14:18:12 -0300

    Formatted setup.sh

--- setup.sh
+++ setup.sh
@@ -5,15 +5,22 @@ RESET_COLOR="\033[0m"
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 
-echo "${GREEN}Create and activate python virtual environment${RESET_COLOR}"
+echo "${GREEN}INFO: ${RESET_COLOR}Creating python virtual environment..."
 python3 -m venv .venv
-source .venv/bin/activate
+echo "${GREEN}INFO: ${RESET_COLOR}Python virtual environment created successfully!"
 
-echo "${GREEN}Setup intial .env file${RESET_COLOR}"
+
+echo "${GREEN}INFO: ${RESET_COLOR}Activating python virtual environment..."
+source .venv/bin/activate
+echo "${GREEN}INFO: ${RESET_COLOR}Python virtual environment activated successfully!"
+
+echo "${GREEN}INFO: ${RESET_COLOR}Setting up intial .env file..."
 cp .env.example .env
 
-echo "${GREEN}Install python dependencies${RESET_COLOR}"
+echo "${GREEN}INFO: ${RESET_COLOR}Installing python dependencies..."
 pip3 install -r requirements.txt
+echo "${GREEN}INFO: ${RESET_COLOR}Python dependencies installed successfully!"
 
-echo "${GREEN}Setup completed successfully! Your environment is ready.${RESET_COLOR}"
-echo "\n${GREEN}Remember to change .env variables to customize your settings.${RESET_COLOR}"
+
+echo "${GREEN}INFO: ${RESET_COLOR}Setup completed successfully! Your environment is ready."
+echo "\n${GREEN}INFO: ${RESET_COLOR}Remember to change .env variables to customize your settings."
